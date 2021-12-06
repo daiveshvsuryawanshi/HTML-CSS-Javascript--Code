512264c8d492566cb79521d8e391172a422ed06c
Daivesh Suryawanshi <daiveshsury@gmail.com>
2021-12-06 08:58:17 +0530

    Login-Page

---
+++ login/script.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+  var pass = document.getElementById("password");
+  if (pass.type === "password") {
+    pass.type = "text";
+  } else {
+    pass.type = "password";
+  }
+}
